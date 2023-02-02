ba52a1002a2a605ce6e6f8c9538a78dc5bf3ab1b
dylan <dylan.sauvat@laplateforme.io>
2023-02-02 13:28:30 +0100

    Starter, problème de connexion BD

--- src/articles.php
+++ src/articles.php
@@ -1,4 +1,6 @@
 <?php
+    // Connexion à la base de données
+    $conn = mysqli_connect("mysql:host=127.0.0.1", "root", "", "dbname=blog_voyage;charset=utf8");
     $bdd = new PDO("mysql:host=127.0.0.1;dbname=blog_voyage;charset=utf8", "root", "");
     if(isset($_POST['id']) && !empty($_POST['id'])) {
        $get_id = htmlspecialchars($_POST['id']);
@@ -14,7 +16,21 @@
     } else {
        die('Erreur d\'affichage.');
     }
+      // Requête SQL pour récupérer les articles
+      $sql = "SELECT * FROM articles ORDER BY date DESC";
+      $result = mysqli_query($conn, $sql);
+  
+      // Boucle pour afficher chaque article
+      while ($articles = mysqli_fetch_assoc($result)) {
+        echo "<h2>" . $articles['titre'] . "</h2>";
+        echo "<p>" . $articles['contenu'] . "</p>";
+        echo "<p>Publié le " . $articles['date'] . "</p>";
+      }
+  
+      // Fermeture de la connexion à la base de données
+      mysqli_close($conn);
     ?>
+    
     <!DOCTYPE html>
     <html lang="fr">
     <head>
@@ -26,28 +42,9 @@
     <h1><?= $titre ?></h1>
        <p><?= $contenu ?></p>
 
-    
   <style>
   </style>
 <body>
   <h1>Articles</h1>
-  <?php
-    // Connexion à la base de données
-    $conn = mysqli_connect("mysql:host=127.0.0.1", "root", "", "dbname=blog_voyage;charset=utf8");
-
-    // Requête SQL pour récupérer les articles
-    $sql = "SELECT * FROM articles ORDER BY date DESC";
-    $result = mysqli_query($conn, $sql);
-
-    // Boucle pour afficher chaque article
-    while ($articles = mysqli_fetch_assoc($result)) {
-      echo "<h2>" . $articles['titre'] . "</h2>";
-      echo "<p>" . $articles['contenu'] . "</p>";
-      echo "<p>Publié le " . $articles['date'] . "</p>";
-    }
-
-    // Fermeture de la connexion à la base de données
-    mysqli_close($conn);
-  ?>
 </body>
 </html>
