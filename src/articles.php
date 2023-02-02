721ad8baec46bdf4ba49114d8ce6e475180a0314
dylan <dylan.sauvat@laplateforme.io>
2023-02-02 13:31:38 +0100

    Starter, problème de connexion BD

--- src/articles.php
+++ src/articles.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <h1><?= $titre ?></h1>
+       <p><?= $contenu ?></p>
+
+  <style>
+  </style>
+<body>
+  <h1>Articles</h1>
+</body>
+</html>
+
 <?php
     // Connexion à la base de données
     $conn = mysqli_connect("mysql:host=127.0.0.1", "root", "", "dbname=blog_voyage;charset=utf8");
@@ -30,21 +48,5 @@
       // Fermeture de la connexion à la base de données
       mysqli_close($conn);
     ?>
-    
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <h1><?= $titre ?></h1>
-       <p><?= $contenu ?></p>
 
-  <style>
-  </style>
-<body>
-  <h1>Articles</h1>
-</body>
-</html>
+    
