49e3781e8d10903577f261a2a0c7493825ad352b
dylan <dylan.sauvat@laplateforme.io>
2023-02-03 11:35:42 +0100

    Starter, problème de connexion à la BD

--- articles.php
+++ articles.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <h1><?= $titre ?></h1>
-       <p><?= $contenu ?></p>
-
-  <style>
-  </style>
-<body>
-  <h1>Articles</h1>
-</body>
-</html>
-
 <?php
     // Connexion à la base de données
     $bdd = new PDO("mysql:host=127.0.0.1;dbname=blog_voyage;charset=utf8", "root", "");
@@ -48,4 +30,20 @@
       mysqli_close($conn);
     ?>
 
-    
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <h1><?= $titre ?></h1>
+       <p><?= $contenu ?></p>
+
+  <style>
+  </style>
+<body>
+  <h1>Articles</h1>
+</body>
+</html>
