2f3dcea9e546ad5c90139278200beb6b94aa8dd9
dylan <dylan.sauvat@laplateforme.io>
2023-02-03 12:24:49 +0100

    Ajout de style , problème à la Bd persiste.

--- articles.php
+++ articles.php
@@ -35,22 +35,82 @@ try {
 }
 ?>
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
 <body>
+<style>
+        h1 {
+            font-size: 36px;
+            text-align: center;
+            margin-top: 50px;
+        }
+
+        p {
+            font-size: 18px;
+            line-height: 1.5;
+            padding: 20px;
+            text-align: justify;
+        }
+
+        .articles {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            margin-top: 50px;
+        }
+
+        .article {
+            width: 30%;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 50px;
+            text-align: center;
+        }
+
+        .article h2 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .article p {
+            font-size: 16px;
+            line-height: 1.5;
+            margin-bottom: 20px;
+        }
+
+        .article a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #000f08;
+            color: #f4fff8;
+            border-radius: 25px;
+            text-decoration: none;
+        }
+    </style>
     <h1><?= $titre ?></h1>
     <p><?= $contenu ?></p>
 
-  <style>
-  </style>
-<body>
   <h1>Articles</h1>
+  <div class="articles">
+    <?php foreach ($articles as $article) : 
+      ?>
+        <div class="article">
+            <h2><?= htmlspecialchars($article['titre']) ?></h2>
+            <p><?= htmlspecialchars($article['contenu']) ?></p>
+            <a href="#">Lire plus</a>
+        </div>
+    <?php endforeach;
+     ?>
+  </div>
 </body>
 </html>
+
 
