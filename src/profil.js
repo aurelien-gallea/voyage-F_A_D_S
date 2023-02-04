5051c614593b4bd98d9e86cd71889e3eeed1f8e2
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-02-04 14:00:19 +0100

    maj profil ok + commmentaires

--- src/profil.js
+++ src/profil.js
@@ -1,25 +1,33 @@
-const block1 = document.querySelector('#block1');
-const block2 = document.querySelector('#block2');
-const password = document.querySelector("#password");
-const pass1 = document.querySelector('#passChange1');
-const pass2 = document.querySelector('#passChange2');
-const pass3 = document.querySelector('#passChange3');
+// formulaire n 1
+const block1 = document.querySelector('#block1'); 
+const password = document.querySelector("#password"); 
 const divBtn = document.querySelector("#divBtn"); // div du button 1er formulaire
-const divBtn2 = document.querySelector("#divBtn2"); // div du button 2e formaulaire
 const btn = document.querySelector('#btn'); // boutton 1er formulaire
+const passWindow = document.querySelector('#passWindow'); // lien vers le 2e form
+
+// formulaire n 2
+const block2 = document.querySelector('#block2'); 
+const pass1 = document.querySelector('#passChange1'); // ancien mdp
+const pass2 = document.querySelector('#passChange2'); // nouveau mdp
+const pass3 = document.querySelector('#passChange3'); // confirmer nouveau mdp
+const notMatch = document.querySelector('#notMatch'); // message d'erreur form 2
+const divBtn2 = document.querySelector("#divBtn2"); // div du button 2e formaulaire
 const btn2 = document.querySelector('#btn2'); // boutton 2e formulaire
-const passWindow = document.querySelector('#passWindow');
-const userWindow = document.querySelector('#userWindow');
+const userWindow = document.querySelector('#userWindow'); // lien vers le 1er form
+
+const regexPass = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  );
 
 block2.style.display = "none"; // on cache par défaut le block2: le changement de mdp
 
-
+// activation du boutton
 function enabledBtn(button,divButton) {
     button.disabled = false;
     button.style.cursor = "pointer";
     divButton.style.filter = "opacity(1)";
 }
-
+// desactivation du boutton
 function disabledBtn(button,divButton) {
     button.disabled = true;
     button.style.cursor = "not-allowed";
@@ -29,29 +37,44 @@ function disabledBtn(button,divButton) {
 disabledBtn(btn,divBtn); // état initial du boutton 1
 disabledBtn(btn2,divBtn2); // état initial du boutton 2
 
+
+// on désactive le 1er formulaire tant que le mdp ne respecte pas le regex 
+// qu'on a fixé au départ lors de l'inscription
+
 password.addEventListener('keyup', () => {
-    if(password.value.length >= 2) {
+    if(regexPass.test(password.value)) {
         enabledBtn(btn,divBtn);
     } else {
         disabledBtn(btn,divBtn);
     }
 });
 
-function disabledForm2(nameInput) {
-    nameInput.addEventListener('keyup', () => {
-        if(pass1.value.length >=2 && pass2.value.length >= 2 && pass3.value.length >= 2) {
+// on affiche/cache un message d'erreur si les mdp ne correspondent pas
+function passMatching() {
+    if(pass2.value == pass3.value) {
+        notMatch.style.visibility = "hidden";
+    } else {
+        notMatch.style.visibility = "visible";
+    }
+}
+// pareil on désactive le 2e formulaire tant que le mdp ne respecte pas le regex
+function disabledForm2(nameInput, event) {
+    nameInput.addEventListener(event, () => {
+        
+        passMatching();
+
+        if(regexPass.test(pass1.value) && regexPass.test(pass2.value) && regexPass.test(pass3.value) && (pass2.value == pass3.value)) {
+            
             enabledBtn(btn2, divBtn2);
+
         } else {
+            
             disabledBtn(btn2,divBtn2);
         }
     });
 }
 
-disabledForm2(pass1);
-disabledForm2(pass2);
-disabledForm2(pass3);
-
-//le block2 qui correspond à la maj du mot de passe
+//le lien qui fait apparaitre le 2e formulaire et disparaitre le 1er
 passWindow.addEventListener('click', (e) => {
     e.preventDefault();
     block2.style.display = "block";
@@ -59,10 +82,16 @@ passWindow.addEventListener('click', (e) => {
     block1.style.display = "none";
 });
 
-//le block1 qui correspond à la maj de login / email
+//le lien qui fait apparaitre le 1e formulaire et disparaitre le 2eme
 userWindow.addEventListener('click', (e) => {
     e.preventDefault();
     block2.style.display = "none";
     block1.style.display = "block";
 
 });
+
+// on est à l'affût du moindre changement dans les inputs 
+// pour désactiver le formulaire
+disabledForm2(pass1,'keyup');
+disabledForm2(pass2,'keyup');
+disabledForm2(pass3,'keyup');
