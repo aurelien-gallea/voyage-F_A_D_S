3d4392b90ae1c0fa9fd9d253f46888e5bb9eb7bb
Sylvia <sylvia.terren@laplateforme.io>
2023-02-09 16:01:11 +0100

    erreur ne marche pas

--- creer-article.php
+++ creer-article.php
@@ -33,7 +33,8 @@ if (isset($_POST['article_titre'], $_POST['article_contenu'], $_POST['categorie'
 		exit; //redirection pour empécher le renvoie du formulaire via f5 et bug
 		//
 	} else { //Si le texte ou/et le titre/catégories ne sont pas rentrés, alors ça affiche une erreur
-		$message = 'Veuillez remplir tous les champs pour compléter la création de l\'article.';
+		header('location:creer-article.php?success=0');
+		exit;
 	}
 } ?>
 
@@ -42,6 +43,7 @@ if (isset($_POST['article_titre'], $_POST['article_contenu'], $_POST['categorie'
 <html lang="fr">
 
 <!-- code pour le header -->
+
 <head>
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -105,47 +107,81 @@ if (isset($_POST['article_titre'], $_POST['article_contenu'], $_POST['categorie'
 	</style>
 </head>
 
-<!-- ----------------------------------body------------------------------->
+<!-------------------------------------body------------------------------->
 
-<body class="flex flex-col min-h-screen">
+<body>
 
 	<!----------------------------- Header ----------------------------------->
 	<?php
 	include 'src/header.php';
 	?>
 	<!-------------------------------- Page -------------------------------------->
+	<section class="flex-grow">
+		<div class="mt-32 mb-10">
+			<!---------------------- Formulaire d'article ---------------------->
+			<form method="POST">
+				<input type="text" name="article_titre" maxlength="80" placeholder="Titre" /><br />
+				<textarea name="article_contenu" maxlength="5000" placeholder="Contenu de l'article..."></textarea><br />
+				<!--Bouton catégorie avec le menu déroulant-->
+				<button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Catégories<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+					</svg></button>
+				<!-- Dropdown menu -->
+				<div id="dropdownSearch" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
+					<?php update::listOfCategories(); ?>
+				</div>
+				<br>
+				<br>
+				<br>
+				<input type="submit" value="Valider" />
+			</form>
 
-	<section class="flex-grow ">
-		<!---------------------- Formulaire d'article ---------------------->
-		<form method="POST">
-			<input type="text" name="article_titre" maxlength="80" placeholder="Titre" /><br />
-			<textarea name="article_contenu" maxlength="5000" placeholder="Contenu de l'article..."></textarea><br />
-			<!--Bouton catégorie avec le menu déroulant-->
-			<button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Catégories<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-				</svg></button>
-			<!-- Dropdown menu -->
-			<div id="dropdownSearch" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
-				<?php update::listOfCategories(); ?>
-			</div>
-			<br>
-			<input type="submit" value="Valider" />
-		</form>
+			<!----------------- Si article bien envoyé : ------------------->
+			<?php
+			if (isset($_GET['success']) && $_GET['success'] == 1) {
+				$message = 'Votre article est maintenant disponible sur le blog. Allons voir si quelqu\'un y a répondu !';
+				echo $message;
+				//Bouton aller sur l'article et bouton retourner au blog
+				//Bouton creer un nouvel article ?
+			} elseif (isset($_GET['success']) && $_GET['success'] == 0) { 		
+				$message2 = 'Veuillez remplir tous les champs pour compléter la création de l\'article.';
+				echo $message2;
+				}?>
 
-		<!----------------- Si article bien envoyé : ------------------->
-		<?php
-		$message = 'Votre article est maintenant disponible sur le blog. Allons voir si quelqu\'un y a répondu !';
-		if (isset($_GET['success']) && $_GET['success'] == 1) {
-			echo $message;
-			//Bouton aller sur l'article et bouton retourner au blog
-			//Bouton creer un nouvel article ?
-		} ?>
+			<article>
+				<p>
+					blop
+				</p>
+			</article>
+		</div>
 	</section>
 
 	<!----------------------------------- Footer ------------------------------------>
-	<?php
-	include 'src/footer.php';
-	?>
+	<footer class="fixed inset-x-0 bottom-0 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+				<img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+			</a>
+			<ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+				</li>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+				</li>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+				</li>
+				<li>
+					<a href="#" class="hover:underline">Contact</a>
+				</li>
+			</ul>
+		</div>
+		<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+		<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 3 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+		</span>
+	</footer>
 	<!-- code du footer -->
 	<script>
 		var themeToggleDarkIcon = document.getElementById(
@@ -195,7 +231,7 @@ if (isset($_POST['article_titre'], $_POST['article_contenu'], $_POST['categorie'
 			}
 		});
 	</script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js%22%3E</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 </body>
 
 </html>
