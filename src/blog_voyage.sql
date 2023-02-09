887d602a74d895807bd57e1525486ff0d7b5988f
dylan <dylan.sauvat@laplateforme.io>
2023-02-09 09:42:26 +0100

    MIse à jour de la base de donnée

--- src/blog_voyage.sql
+++ src/blog_voyage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 03 fév. 2023 à 23:44
+-- Généré le : jeu. 09 fév. 2023 à 09:40
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.2.0
 
@@ -32,7 +32,6 @@ CREATE TABLE `articles` (
   `titre` varchar(80) NOT NULL,
   `article` text NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
-  `id_categorie` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,10 +39,18 @@ CREATE TABLE `articles` (
 -- Déchargement des données de la table `articles`
 --
 
-INSERT INTO `articles` (`id`, `titre`, `article`, `id_utilisateur`, `id_categorie`, `date`) VALUES
-(1, 'hello', 'bonjour tous le monde', 32, 1, '2023-02-03 21:34:26'),
-(9, 'hello', 'bonjour tous le monde', 32, 1, '2023-02-03 21:58:11'),
-(10, 'hlo', 'bonjour tous le me', 32, 2, '2023-02-03 22:00:39');
+INSERT INTO `articles` (`id`, `titre`, `article`, `id_utilisateur`, `date`) VALUES
+(31, 'ceci est un nouveau titre', 'une belle histoire après tout', 33, '2023-02-07 21:09:48'),
+(32, 'new title', 'plongez au coeur des glaciers de palawajoctarunu', 33, '2023-02-07 20:54:02'),
+(33, 'test', 'test3', 33, '2023-02-07 10:51:02'),
+(34, 'je change le titre', 'test4 alors là tu vois j&#039;ai changé les catégories', 33, '2023-02-07 21:12:29'),
+(35, 'test', 'test5', 33, '2023-02-07 10:51:02'),
+(36, 'test1', 'blablablaAAAAAAAAAAA', 51, '2023-02-08 10:59:43'),
+(37, 'test22', 'asdcxozkfjzffsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 51, '2023-02-08 11:03:55'),
+(38, 'test1', 'blablabla', 51, '2023-02-08 10:59:07'),
+(39, 'test1', 'blablabla', 51, '2023-02-08 10:59:07'),
+(40, 'test1', 'blablabla', 51, '2023-02-08 10:59:07'),
+(41, 'test1', 'blablabla', 51, '2023-02-08 10:59:07');
 
 -- --------------------------------------------------------
 
@@ -82,7 +89,18 @@ CREATE TABLE `cat_art` (
 --
 
 INSERT INTO `cat_art` (`id_art`, `id_cat`) VALUES
-(1, 1);
+(33, 3),
+(35, 1),
+(35, 2),
+(34, 2),
+(31, 4),
+(32, 1),
+(32, 4),
+(37, 1),
+(37, 2),
+(37, 3),
+(37, 4),
+(36, 3);
 
 -- --------------------------------------------------------
 
@@ -103,8 +121,10 @@ CREATE TABLE `commentaires` (
 --
 
 INSERT INTO `commentaires` (`id`, `commentaire`, `id_article`, `id_utilisateur`, `date`) VALUES
-(3, 'very good value, input , input', 1, 32, '2023-02-03 22:40:22'),
-(4, 'this is beautiful', 1, 32, '2023-02-03 22:43:42');
+(5, 'ceci est mon 1er commentaire', 37, 52, '2023-02-08 19:10:05'),
+(6, 'ceci est mon 1er commentaire', 37, 52, '2023-02-08 19:10:11'),
+(7, 'ceci est mon 1er commentaire', 37, 52, '2023-02-08 19:10:12'),
+(8, 'ceci est mon 1er commentaire', 37, 52, '2023-02-08 19:10:30');
 
 -- --------------------------------------------------------
 
@@ -135,7 +155,16 @@ INSERT INTO `droits` (`id`, `nom`, `id_utilisateur`) VALUES
 (28, 'membre', 41),
 (29, 'admin', 42),
 (30, 'membre', 43),
-(31, 'membre', 44);
+(31, 'membre', 44),
+(32, 'membre', 45),
+(33, 'membre', 46),
+(34, 'membre', 47),
+(35, 'membre', 48),
+(36, 'membre', 49),
+(37, 'membre', 50),
+(38, 'membre', 51),
+(39, 'membre', 52),
+(40, 'membre', 53);
 
 -- --------------------------------------------------------
 
@@ -167,7 +196,16 @@ INSERT INTO `utilisateurs` (`id`, `login`, `password`, `email`) VALUES
 (41, 'aa', 'e4f62f77152f5f843091e1013214eb8054591917312d4b', 'lolo@aa.aa'),
 (42, 'tomy', 'e4f488014676aeb3ff3b93c588c35d2e75a54a8ccb6d4b', 'qs@wxs.qz'),
 (43, 'aa', 'e4fa0a243467a19eee94f25614618458689b1e0063bd4b', 'zz@zz.zz'),
-(44, 'alfred', 'e4fa0a243467a19eee94f25614618458689b1e0063bd4b', 'zz@ds.dr');
+(44, 'alfred', 'e4fa0a243467a19eee94f25614618458689b1e0063bd4b', 'zz@ds.dr'),
+(45, 'aaa', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'aa@aa.aaa'),
+(46, 'bobol', 'e4f48bb569634cfc0c0c9e6c50616ba06ab591ca621d4b', 'aa@zz.qq'),
+(47, 'boboull', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'aa@qq.qq'),
+(48, '&lt;script&gt;alert(&#039;ok&#039;)&lt;/script&gt;', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'aa@aa.aaqq'),
+(49, 'ùùééééééé`è', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'aa@qq.com'),
+(50, 'aaq', 'e4fe248b8b6d9747ea6ff4b4769b75914bd8941e33fd4b', 'qq@ww.ss'),
+(51, 'avrel', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'avrel@aa.aa'),
+(52, 'xaxa', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'xaxa@aa.aa'),
+(53, 'boris', 'e4fc1365e2012646a44d42c67a357eab39db0e37c67d4b', 'boris@aa.aa');
 
 --
 -- Index pour les tables déchargées
@@ -178,21 +216,20 @@ INSERT INTO `utilisateurs` (`id`, `login`, `password`, `email`) VALUES
 --
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`) USING BTREE,
-  ADD KEY `id_categorie` (`id_categorie`) USING BTREE;
+  ADD KEY `id_utilisateur` (`id_utilisateur`) USING BTREE;
 
 --
 -- Index pour la table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `id` (`id`) USING BTREE;
 
 --
 -- Index pour la table `cat_art`
 --
 ALTER TABLE `cat_art`
-  ADD UNIQUE KEY `id_art` (`id_art`) USING BTREE,
-  ADD KEY `id_cat` (`id_cat`) USING BTREE;
+  ADD KEY `id_cat` (`id_cat`),
+  ADD KEY `id_art` (`id_art`) USING BTREE;
 
 --
 -- Index pour la table `commentaires`
@@ -223,7 +260,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT pour la table `categories`
@@ -235,19 +272,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `droits`
 --
 ALTER TABLE `droits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- Contraintes pour les tables déchargées
