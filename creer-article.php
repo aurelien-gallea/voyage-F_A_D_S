1c835da2192aa6f0fbc430d118c79444e1c84af0
Sylvia <sylvia.terren@laplateforme.io>
2023-02-09 14:25:45 +0100

    Test pour creer article : requêtes marchent, frotn en cours, correction des bugs, article-poste ?

--- creer-article.php
+++ creer-article.php
@@ -1,10 +1,10 @@
 <?php
-#On fait croire que l'utilisateur est connecté
+//On fait croire que l'utilisateur est connecté
 session_start();
 
-if(!isset($_SESSION['id'])) {
-    header('location:connexion.php');
-    exit();
+if (!isset($_SESSION['id'])) {
+	header('location:connexion.php');
+	exit();
 }
 
 require("classes/update.php");
@@ -20,65 +20,182 @@ if (isset($_POST['article_titre'], $_POST['article_contenu'], $_POST['categorie'
 		$article_titre = htmlspecialchars($_POST['article_titre']);
 		$article_contenu = htmlspecialchars($_POST['article_contenu']);
 		//Poste l'article 
-		$req = $bdd->prepare('INSERT INTO articles (titre, article, date_post, id_utilisateur) VALUES (?, ?, NOW(), ?)');
+		$req = $bdd->prepare('INSERT INTO articles (titre, article, id_utilisateur) VALUES (?, ?, ?)');
 		$req->execute(array($article_titre, $article_contenu, $_SESSION['id']));
 
+		//lie les catégories choisit à l'article
 		$lastID = $bdd->lastInsertId();
 		foreach ($_POST['categorie'] as $value) {
 			$categorie = htmlspecialchars($value);
 			Update::insertIntoCatArt($lastID, $categorie);
 		}
-
-		$message = 'Votre article est maintenant disponible sur le blog. Allons voir si quelqu\'un y a répondu !';
-	}
-	else { //Si le texte ou/et le titre ne sont pas rentrés, alors ça affiche une erreur
+		header('location:creer-article.php?success=1');
+		exit; //redirection pour empécher le renvoie du formulaire via f5 et bug
+		//
+	} else { //Si le texte ou/et le titre/catégories ne sont pas rentrés, alors ça affiche une erreur
 		$message = 'Veuillez remplir tous les champs pour compléter la création de l\'article.';
 	}
-}; ?>
+} ?>
 
+<!----------------------------------- HTML --------------------------------->
 <!DOCTYPE html>
 <html lang="fr">
+
+<!-- code pour le header -->
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300&display=swap" rel="stylesheet" />
+	<link rel="stylesheet" href="css/voyages.css">
+	<link rel="stylesheet" href="css/stylefooter.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@0.2.28/bundled/lenis.js"></script>
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
-	<title>Créér votre propre article</title>
+	<title>Creez votre article</title>
+	<script>
+		tailwind.config = {
+			darkMode: 'class',
+			// ...
+			content: [],
+			theme: {
+				extend: {
+					colors: {
+						'color-1': '#000F08',
+						'color-2': '#1C3738',
+						'color-3': '#4D4847',
+						'color-4': '#F4FFF8',
+						'color-5': '#8BAAAD',
+					},
+					opacity: {
+						54: ".24",
+					},
+					fontFamily: {
+						Unbounded: ['"Unbounded"'],
+					},
+				},
+			},
+		};
+	</script>
+
+	<script>
+		// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+		if (
+			localStorage.getItem("color-theme") === "dark" ||
+			(!("color-theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+	</script>
+	<style>
+		.bg-custom-1C3738 {
+			background-color: #1c3738;
+		}
+
+		.bg-custom-F4FFF8 {
+			background-color: #f4fff8;
+		}
+	</style>
 </head>
 
-<body>
-	<form method="POST"> <!-- formulaire d'envoi-->
-		<input type="text" name="article_titre" placeholder="Titre" /><br />
-		<textarea name="article_contenu" placeholder="Contenu de l'article..."></textarea><br />
-		<!--Bouton catégorie avec le menu déroulant-->
-		<button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Catégories<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-			</svg></button>
-		<!-- Dropdown menu Je dois faire marcher la barre de recherche -->
-		<div id="dropdownSearch" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
-			<div class="p-3">
-				<label for="input-group-search" class="sr-only">Recherche</label>
-				<div class="relative">
-					<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-						<svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
-						</svg>
-					</div>
-					<input type="text" id="input-group-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Recherchez une catégorie">
-				</div>
+<!-- ----------------------------------body------------------------------->
+
+<body class="flex flex-col min-h-screen">
+
+	<!----------------------------- Header ----------------------------------->
+	<?php
+	include 'src/header.php';
+	?>
+	<!-------------------------------- Page -------------------------------------->
+
+	<section class="flex-grow ">
+		<!---------------------- Formulaire d'article ---------------------->
+		<form method="POST">
+			<input type="text" name="article_titre" maxlength="80" placeholder="Titre" /><br />
+			<textarea name="article_contenu" maxlength="5000" placeholder="Contenu de l'article..."></textarea><br />
+			<!--Bouton catégorie avec le menu déroulant-->
+			<button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Catégories<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+				</svg></button>
+			<!-- Dropdown menu -->
+			<div id="dropdownSearch" class="z-10 hidden bg-white rounded-lg shadow w-60 dark:bg-gray-700">
+				<?php update::listOfCategories(); ?>
 			</div>
-			<?php update::listOfCategories(); ?>
-		</div>
-		<br>
-		<input type="submit" value="Valider" />
-	</form>
+			<br>
+			<input type="submit" value="Valider" />
+		</form>
 
-	<?php if (isset($message)) {
-		echo $message;
-	} ?>
+		<!----------------- Si article bien envoyé : ------------------->
+		<?php
+		$message = 'Votre article est maintenant disponible sur le blog. Allons voir si quelqu\'un y a répondu !';
+		if (isset($_GET['success']) && $_GET['success'] == 1) {
+			echo $message;
+			//Bouton aller sur l'article et bouton retourner au blog
+			//Bouton creer un nouvel article ?
+		} ?>
+	</section>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+	<!----------------------------------- Footer ------------------------------------>
+	<?php
+	include 'src/footer.php';
+	?>
+	<!-- code du footer -->
+	<script>
+		var themeToggleDarkIcon = document.getElementById(
+			"theme-toggle-dark-icon"
+		);
+		var themeToggleLightIcon = document.getElementById(
+			"theme-toggle-light-icon"
+		);
+
+		// Change the icons inside the button based on previous settings
+		if (
+			localStorage.getItem("color-theme") === "dark" ||
+			(!("color-theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			themeToggleLightIcon.classList.remove("hidden");
+		} else {
+			themeToggleDarkIcon.classList.remove("hidden");
+		}
+
+		var themeToggleBtn = document.getElementById("theme-toggle");
+
+		themeToggleBtn.addEventListener("click", function() {
+			// toggle icons inside button
+			themeToggleDarkIcon.classList.toggle("hidden");
+			themeToggleLightIcon.classList.toggle("hidden");
+
+			// if set via local storage previously
+			if (localStorage.getItem("color-theme")) {
+				if (localStorage.getItem("color-theme") === "light") {
+					document.documentElement.classList.add("dark");
+					localStorage.setItem("color-theme", "dark");
+				} else {
+					document.documentElement.classList.remove("dark");
+					localStorage.setItem("color-theme", "light");
+				}
+
+				// if NOT set via local storage previously
+			} else {
+				if (document.documentElement.classList.contains("dark")) {
+					document.documentElement.classList.remove("dark");
+					localStorage.setItem("color-theme", "light");
+				} else {
+					document.documentElement.classList.add("dark");
+					localStorage.setItem("color-theme", "dark");
+				}
+			}
+		});
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js%22%3E</script>
 </body>
 
 </html>
