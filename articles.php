303abcf2d19be0bad40906fbb3e83d22c41f788a
dylan <dylan.sauvat@laplateforme.io>
2023-02-14 15:34:22 +0100

    mise a jour

--- articles.php
+++ articles.php
@@ -38,9 +38,6 @@ if (isset($_POST['tri'])) {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="articles.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
