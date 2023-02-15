6cbe3289d178fbbb2aa8f6b7c3e0d7d9b22f2047
dylan <dylan.sauvat@laplateforme.io>
2023-02-15 13:08:21 +0100

    ajout script

--- usa.php
+++ usa.php
@@ -424,7 +424,8 @@
 				<!-- Scripts -->
 			
 				<script src="assets/js/main.js"></script>
-
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+    <script src="src/tailwind-need-body.js"></script> 
 	</body>
 	<footer class="text-white">
   <link rel="stylesheet" href="stylefooter.css" />
