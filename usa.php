1d974aa7e3453be82679eeebbdca0984bae94fc9
dylan <dylan.sauvat@laplateforme.io>
2023-02-15 10:46:42 +0100

    modif footer

--- usa.php
+++ usa.php
@@ -420,52 +420,52 @@
 				<script src="assets/js/main.js"></script>
 
 	</body>
-	<footer>
-	
-		<div class="main-content">
-		  <div class="left box">
-			<h2>A Propos</h2>
-			<div class="content">
-			  <p>Tasty Trip, site de voyage gourmand.</p>
-			  <div class="social">
-				<a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
-				<a href="#"><span class="fab fa-twitter"></span></a>
-				<a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
-				<a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
-				<a href="https://github.com/c/codingnepal"><span class="fab fa-github"></span></a>
-			  </div>
-			</div>
-		  </div>
-		  <div class="center box">
-			<h2>Addresse</h2>
-			<div class="content">
-			  <div class="place">
-				<span class="fas fa-map-marker-alt"></span>
-				<span class="text">Toulon, France</span>
-			  </div>
-			  <div class="phone">
-				<span class="fas fa-phone-alt"></span>
-				<span class="text">+089-765432100</span>
-			  </div>
-			  <div class="email">
-				<span class="fas fa-envelope"></span>
-				<span class="text">abc@example.com</span>
-			  </div>
-			</div>
-		  </div>
-		  <div class="right box">
-			<h2>Nous Contacter</h2>
-			<div class="content">
-			<a href="contact.php">Nous Contacter ici</a>
-			   
-				</div>
-		  </div>
-		</div>
-		<div class="bottom">
-		
-			<span class="credit">Tasty Trip | </span>
-			<span class="far fa-copyright"></span><span> 2023 All rights reserved.</span>
-		  
-		</div>
-	  </footer>
+	<footer class="text-white">
+  <link rel="stylesheet" href="stylefooter.css" />
+  <div class="main-content">
+    <div class="left box">
+      <h2>A Propos</h2>
+      <div class="content">
+        <p>Tasty Trip, site de voyage gourmand.</p>
+        <div class="social">
+          <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
+          <a href="#"><span class="fab fa-twitter"></span></a>
+          <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
+          <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+          <a href="https://github.com/c/codingnepal"><span class="fab fa-github"></span></a>
+        </div>
+      </div>
+    </div>
+    <div class="center box">
+      <h2>Addresse</h2>
+      <div class="content">
+        <div class="place">
+          <span class="fas fa-map-marker-alt"></span>
+          <span class="text-white ml-2 inline-block">Toulon, France</span>
+        </div>
+        <div class="phone">
+          <span class="fas fa-phone-alt"></span>
+          <span class="text-white ml-2 inline-block">+089-765432100</span>
+        </div>
+        <div class="email">
+          <span class="fas fa-envelope"></span>
+          <span class="text-white ml-2 inline-block">abc@example.com</span>
+        </div>
+      </div>
+    </div>
+    <div class="right box">
+      <h2>Nous Contacter</h2>
+      <div class="content">
+        <a href="contact.php" class="text-white ml-2 inline-block">Nous Contacter ici</a>
+      </div>
+    </div>
+  </div>
+  <div class="bottom">
+  <div class="container">
+    <span class="text-white ml-4 inline-block">Tasty Trip | </span>
+    <span class="text-white inline-block">2023 All rights reserved.</span>
+  </div>
+</div>
+
+</footer>
 </html>
