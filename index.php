3d34d3df8c811e7dd3b095b7cbf78257d2232028
dylan <dylan.sauvat@laplateforme.io>
2023-02-16 16:05:42 +0100

    ajustement

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ session_start();
           extend: {
             colors: {
         'color-1': '#111827',
-        'color-2': '8BAAAD',
+        'color-2': '#8BAAAD',
         'color-3': '##90323D',
         'color-4': '#FCF6B1',
         'color-5': '##F18F01AD',
