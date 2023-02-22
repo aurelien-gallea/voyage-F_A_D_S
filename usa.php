e82bbf9c0f40e81e1560c6bed8eccd5647d4da55
dylan <dylan.sauvat@laplateforme.io>
2023-02-22 12:39:37 +0100

    modif footer

--- usa.php
+++ usa.php
@@ -7,16 +7,19 @@ session_start();
 	<head>
 		<title>USA</title>
 		
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="assets/stylefooter.css" />
-	<link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300&display=swap" rel="stylesheet" />
-   <script src="https://cdn.tailwindcss.com"></script>
+		<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/stylefooter.css">
+    <!-- <link rel="stylesheet" href="css/voyages.css"> -->
     <link rel="stylesheet" href="css/tailwind-need.css">
     <script src="src/tailwind-need.js"></script>
     <link href="assets/favicon.ico" rel="icon" type="image/x-icon" />
+    <link href="assets/ind-favicon.png" rel="icon" type="image/x-icon" />
 		
 	</head>
 	<?php require_once('src/header.php'); ?>
@@ -371,11 +374,10 @@ session_start();
 		
 		
 				<!-- Scripts -->
-			
+				<?php require_once("src/footer.php"); ?>
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
     <script src="src/tailwind-need-body.js"></script>
 	
 	</body>
-	<?php require_once("src/footer.php");
-  ?>
+	
 </html>
